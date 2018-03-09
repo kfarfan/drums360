a294f0f823f7d026a5a87e3ff23bfe896f656cd7
kfarfan <34223564+kfarfan@users.noreply.github.com>
2018-03-09 00:04:46 -0500

    adding sound effect and color scheme

---
+++ assets/js/app.js
@@ -0,0 +1,69 @@
+const a = document.getElementById("a");
+const s = document.getElementById("s");
+const d = document.getElementById("d");
+const f = document.getElementById("f");
+const g = document.getElementById("g");
+const h = document.getElementById("h");
+const j = document.getElementById("j");
+const k = document.getElementById("k");
+const l = document.getElementById("l");
+
+var audio = new Audio("assets/css/sounds/boom.wav");
+var q = new Audio("assets/css/sounds/clap.wav");
+var w = new Audio("assets/css/sounds/hihat.wav");
+var e = new Audio("assets/css/sounds/kick.wav");
+var r = new Audio("assets/css/sounds/openhat.wav");
+var t = new Audio("assets/css/sounds/ride.wav");
+var y = new Audio("assets/css/sounds/snare.wav");
+var u = new Audio("assets/css/sounds/tink.wav");
+var i = new Audio("assets/css/sounds/tom.wav");
+
+document.body.addEventListener('keypress', function(event){
+  if(event.keyCode === 97){
+    audio.play();
+    a.classList.toggle('playing');
+  }
+  if(event.keyCode === 115){
+    q.play();
+    s.classList.toggle('yellow');
+  }
+  if(event.keyCode === 100){
+    w.play();
+    d.classList.toggle('yellow');
+  }
+  if(event.keyCode === 102){
+    e.play();
+    f.classList.toggle('yellow');
+  }
+  if(event.keyCode === 103){
+    r.play();
+    g.classList.toggle('yellow');
+  }
+  if(event.keyCode === 104){
+    t.play();
+    h.classList.toggle('yellow');
+  }
+  if(event.keyCode === 106){
+    y.play();
+    j.classList.toggle('yellow');
+  }
+  if(event.keyCode === 107){
+    u.play();
+    j.classList.toggle('yellow');
+  }
+  if(event.keyCode === 108){
+    i.play();
+    l.classList.toggle('yellow');
+  }
+});
+
+function flicker(){
+  var count = 0;
+  var intId = setInterval(function(){
+    count++;
+    this.keyCode.style.border = "yellow"
+    if(count === 2){
+      clearInterval(intId);
+    }
+},200);
+};
