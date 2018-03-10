9a470e67b11fadd959bd426b8674c2ada5bfd2a5
kfarfan <34223564+kfarfan@users.noreply.github.com>
2018-03-09 20:56:40 -0500

    correcting audio sync

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,38 +20,47 @@ var i = new Audio("assets/css/sounds/tom.wav");
 
 document.body.addEventListener('keypress', function(event){
   if(event.keyCode === 97){
+    audio.currentTime = 0;
     audio.play();
     a.classList.toggle('playing');
   }
   if(event.keyCode === 115){
+    q.currentTime = 0;
     q.play();
     s.classList.toggle('playing');
   }
   if(event.keyCode === 100){
+    w.currentTime = 0;
     w.play();
     d.classList.toggle('playing');
   }
   if(event.keyCode === 102){
+    e.currentTime = 0;
     e.play();
     f.classList.toggle('playing');
   }
   if(event.keyCode === 103){
+    r.currentTime = 0;
     r.play();
     g.classList.toggle('playing');
   }
   if(event.keyCode === 104){
+    t.currentTime = 0;
     t.play();
     h.classList.toggle('playing');
   }
   if(event.keyCode === 106){
+    y.currentTime = 0;
     y.play();
     j.classList.toggle('playing');
   }
   if(event.keyCode === 107){
+    u.currentTime = 0;
     u.play();
-    j.classList.toggle('playing');
+    k.classList.toggle('playing');
   }
   if(event.keyCode === 108){
+    i.currentTime = 0;
     i.play();
     l.classList.toggle('playing');
   }
